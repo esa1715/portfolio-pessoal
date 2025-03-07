331e39d6be461edb89388186484952c410e9e0ab
esa1715 <silvalveserik1@gmail.com>
2025-03-07 19:33:46 -0300

    Adições e ajustes ao Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAt } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedinIn, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
 const Header = () => {
@@ -10,9 +11,14 @@ const Header = () => {
                 <h1 className='header__tit'>Erik da Silva Alves</h1>
                 <h2 className='header__sub'>Desenvolvedor Front-end Júnior</h2>
                 <div className='header__cont'>
-                    <h2>Contatos</h2>
+                    <h2>Redes Sociais</h2>
                     <div className='header__cont__icons'>
                         <ul>
+                            <li>
+                                <a href='https://mail.google.com/mail/?view=cm&fs=1&to=silvalveserik1@gmail.com&su=Portfólio%20pessoal%20' target="_blank" rel="noreferrer">
+                                <FontAwesomeIcon icon={faAt} size="2xl" style={{color: "#ffed7e",}} />
+                                </a>
+                            </li>
                             <li>
                                 <a href='https://github.com/esa1715' target="_blank" rel="noreferrer">
                                 <FontAwesomeIcon icon={faGithub} size='2xl' style={{color: "#ffed7e",}} />
