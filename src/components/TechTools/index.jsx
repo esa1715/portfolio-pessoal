aad0f7ca5d08d3ecbc8670f402729491d2c54e15
esa1715 <silvalveserik1@gmail.com>
2025-03-08 20:25:00 -0300

    Ajuste logos TechTools

--- src/components/TechTools/index.jsx
+++ src/components/TechTools/index.jsx
@@ -7,39 +7,39 @@ const TechTools = () => {
             <h2 className='techTools__tit'>TECNOLOGIAS E FERRAMENTAS</h2>
             <div className='techtools__icons'>
                 <div className='icon__content'>
-                    <img src="src/assets/html-5.png" alt="Logo HTML-5" />
+                    <img src='/assets/html-5.png' alt="Logo HTML-5" />
                     <span>HTML</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/css-3.png" alt="Logo CSS-3" />
+                    <img src='/assets/css-3.png' alt="Logo CSS-3" />
                     <span>CSS</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/javascript.png" alt="Logo JavaScript" />
+                    <img src='/assets/javascript.png' alt="Logo JavaScript" />
                     <span>JavaScript</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/github.png" alt="Logo GitHub" />
+                    <img src='/assets/github.png' alt="Logo GitHub" />
                     <span>GitHub</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/react.png" alt="Logo React" />
+                    <img src='/assets/react.png' alt="Logo React" />
                     <span>React</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/excel.png" alt="Logo Excel" />
+                    <img src='/assets/excel.png' alt="Logo Excel" />
                     <span>Excel</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/powerpoint.png" alt="Logo PowerPoint" />
+                    <img src='/assets/powerpoint.png' alt="Logo PowerPoint" />
                     <span>PowerPoint</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/word.png" alt="Logo Word" />
+                    <img src='/assets/word.png' alt="Logo Word" />
                     <span>Word</span>
                 </div>
                 <div className='icon__content'>
-                    <img src="src/assets/powerbi.png" alt="Logo Power BI" />
+                    <img src='/assets/powerbi.png' alt="Logo Power BI" />
                     <span>Power BI</span>
                 </div>
             </div>
