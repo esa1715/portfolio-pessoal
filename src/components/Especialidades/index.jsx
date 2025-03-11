2a0b2f3edf2c6da2fa7d56454d5618de21f9f054
esa1715 <silvalveserik1@gmail.com>
2025-03-11 15:45:37 -0300

    Adiciona imagens ao Especialidades

--- src/components/Especialidades/index.jsx
+++ src/components/Especialidades/index.jsx
@@ -5,14 +5,17 @@ import { faMobile,faRotateLeft } from '@fortawesome/free-solid-svg-icons'
 const Especialidades = () => {
     const especialidades = [
         {
+            "img": "/assets/react.png",
             "nome": "React",
             "descricao": "Desenvolver aplicações robustas em React, priorizando a reutilização de componentes e a organização do código para garantir manutenção eficiente e escalabilidade."
         },
         {
+            "img": "/assets/responsividade.png",
             "nome": "Responsividade",
             "descricao": "Criar layouts totalmente responsivos, garantindo adaptação fluida a diferentes dispositivos, desde desktops até smartwatches."
         },
         {
+            "img": "/assets/interatividadevisual.png",
             "nome": "Design Interativo",
             "descricao": "Desenvolver interações dinâmicas e envolventes, como animações e efeitos de hover, para aprimorar a experiência do usuário."
         }        
@@ -28,6 +31,7 @@ const Especialidades = () => {
             <ul>
                 {especialidades.map((especialidades, index) => (
                     <li key={index} className='especialidades'>
+                        <img src={especialidades.img}/>
                         <h3>{especialidades.nome}</h3>
                         <p>{especialidades.descricao}</p>
                     </li>
