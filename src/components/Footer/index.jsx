ccd4e74a4372386ca0e44e5327397fdea915465e
esa1715 <silvalveserik1@gmail.com>
2025-03-12 22:01:25 -0300

    Adiciona componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <section className='footer'>
+            <span>©2025 Erik da Silva Alves. Todos os direitos reservados.</span>
+        </section>
+    )
+}
+
+export default Footer
