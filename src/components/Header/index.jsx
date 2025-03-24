1e23b6db3dd3c29d4d8420fc9e80dc966595ed47
esa1715 <silvalveserik1@gmail.com>
2025-03-23 22:14:23 -0300

    Pequenos ajustes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <header>
             <div className='header__content'>
-                <img src="https://avatars.githubusercontent.com/u/189414987?v=4" alt="Foto de perfil de Erik da Silva Alves" className='header__avatar'/>
+                <img src="https://avatars.githubusercontent.com/u/189414987?v=4" alt="Foto de perfil de Erik" className='header__avatar'/>
                 <h1 className='header__tit'>ERIK DA SILVA ALVES</h1>
                 <h2 className='header__sub'>DESENVOLVEDOR FRONT-END JÚNIOR</h2>
                 <div className='header__redes'>
@@ -15,22 +15,22 @@ const Header = () => {
                     <ul>
                         <li>
                             <a href='https://mail.google.com/mail/?view=cm&fs=1&to=silvalveserik1@gmail.com&su=Portfólio%20pessoal%20' target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faAt} size="2xl" className="icon" />
+                            <FontAwesomeIcon icon={faAt} size="2xl" className="icon" alt="Email pessoal de Erik" />
                             </a>
                         </li>
                         <li>
                             <a href='https://github.com/esa1715' target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faGithub} size="2xl" className="icon" />
+                            <FontAwesomeIcon icon={faGithub} size="2xl" className="icon" alt="Página de perfil de Erik no GitHub" />
                             </a>
                         </li>
                         <li>
                             <a href='https://www.linkedin.com/in/erikalves12' target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faLinkedinIn} size="2xl" className="icon" />
+                            <FontAwesomeIcon icon={faLinkedinIn} size="2xl" className="icon" alt="Página de perfil de Erik no LinkedIn" />
                             </a>    
                         </li>
                         <li>
                             <a href='https://api.whatsapp.com/send/?phone=%2B5511933329021&text&type=phone_number&app_absent=0' target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faWhatsapp} size="2xl" className="icon" />
+                            <FontAwesomeIcon icon={faWhatsapp} size="2xl" className="icon" alt="Link para o WhatsApp pessoal de Erik" />
                             </a>
                         </li>
                     </ul>
